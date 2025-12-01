f754d0f4628d1af58dd5ad6518198b1c7ec03dab
ViniciusOLucio <vinicius.olucio@gmail.com>
2025-12-01 10:47:55 -0300

    aviation api

---
+++ app/Filament/Admin/Pages/Aviation.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Admin\Pages;
+
+use Filament\Pages\Page;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+use function Laravel\Prompts\search;
+
+class Aviation extends Page implements HasTable
+{
+    use InteractsWithTable;
+
+    protected string $view = 'filament.admin.pages.aviation';
+
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->records(function () {
+
+                $data = cache()->remember('aviation_flights', 100, function () {
+
+                    $response = Http::get('http://api.aviationstack.com/v1/flights', [
+                        'access_key' => config('services.aviation.key'),
+//                        'limit' => 100,
+//                        'total' => 1,
+                    ]);
+
+                    if ($response->failed()) {
+                        return [];
+                    }
+
+                    return $response->json('data') ?? [];
+                });
+
+
+              return $data;
+
+//                $page = request()->integer('page', 1);
+//                $perPage = request()->integer('per_page', 5);
+//
+//                $offset = ($page - 1) * $perPage;
+//
+//                $items = array_slice($data, $offset, $perPage);
+//
+//                return new LengthAwarePaginator(
+//                    $items,
+//                    count($data),
+//                    $perPage,
+//                    $page,
+//                    [
+//                        'path' => request()->url(),
+//                        'query' => request()->query(),
+//                    ]
+//                );
+            })
+
+
+            ->columns([
+                TextColumn::make('flight_date')
+                    ->dateTime(timezone: 'America/Sao_Paulo'),
+                TextColumn::make('airline.name'),
+                TextColumn::make('flight.number'),
+                TextColumn::make('departure.airport'),
+                TextColumn::make('arrival.airport'),
+            ]);
+
+//            ->defaultPaginationPageOption(5)
+//            ->paginated([5, 10, 25, 50, 100, 'all']);
+    }
+
+
+
+}
--- config/services.php
+++ config/services.php
@@ -14,6 +14,10 @@ return [
     |
     */
 
+    'aviation' => [
+      'key' => env('AVIATION_API_KEY')
+    ],
+
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
     ],
