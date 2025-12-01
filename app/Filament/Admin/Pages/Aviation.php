0a540ad412c5f4a51af59dec882afe1299b24985
ViniciusOLucio <vinicius.olucio@gmail.com>
2025-12-01 14:41:27 -0300

    feat: wip

--- app/Filament/Admin/Pages/Aviation.php
+++ app/Filament/Admin/Pages/Aviation.php
@@ -9,13 +9,9 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
-use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Str;
-use function Laravel\Prompts\search;
 
 class Aviation extends Page implements HasTable
 {
@@ -33,8 +29,8 @@ class Aviation extends Page implements HasTable
 
                     $response = Http::get('http://api.aviationstack.com/v1/flights', [
                         'access_key' => config('services.aviation.key'),
-//                        'limit' => 100,
-//                        'total' => 1,
+                        'limit' => 100,
+                        'total' => 1,
                     ]);
 
                     if ($response->failed()) {
@@ -44,62 +40,41 @@ class Aviation extends Page implements HasTable
                     return $response->json('data') ?? [];
                 });
 
-
-              return $data;
-
-//                $page = request()->integer('page', 1);
-//                $perPage = request()->integer('per_page', 5);
-//
-//                $offset = ($page - 1) * $perPage;
-//
-//                $items = array_slice($data, $offset, $perPage);
-//
-//                return new LengthAwarePaginator(
-//                    $items,
-//                    count($data),
-//                    $perPage,
-//                    $page,
-//                    [
-//                        'path' => request()->url(),
-//                        'query' => request()->query(),
-//                    ]
-//                );
+                return $data;
             })
-
-
             ->columns([
                 TextColumn::make('flight_date')
-                    ->dateTime(timezone: 'America/Sao_Paulo'),
-                TextColumn::make('airline.name'),
-                TextColumn::make('flight.number'),
-                TextColumn::make('departure.airport'),
-                TextColumn::make('arrival.airport'),
+                    ->label('Data do voo')
+                    ->dateTime('D/M/Y'),
+
+                TextColumn::make('airline.name')
+                    ->label('Companhia aérea'),
+
+                TextColumn::make('departure.airport')
+                    ->label('Partida'),
+
+                TextColumn::make('arrival.airport')
+                    ->label('Chegada'),
+
+                TextColumn::make('flight.number')
+                    ->label('Voo'),
             ])
             ->headerActions([
                 Action::make('cache_clear')
                     ->label('Limpar Cache')
                     ->color('danger')
                     ->requiresConfirmation()
-                    ->action(function(){
-//                        $this->refresh();
+                    ->action(function () {
                         Cache::flush();
                         Artisan::call('cache:clear');
                         Notification::make()
                             ->success()
                             ->title('Cache limpo')
                             ->body('Novos dados foram carregados')
-                        ->send();
+                            ->send();
                     }),
-
-
-
             ]);
-
-
-//            ->defaultPaginationPageOption(5)
-//            ->paginated([5, 10, 25, 50, 100, 'all']);
     }
-
 
 
 }
